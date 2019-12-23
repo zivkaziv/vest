bb4ce841153ed2bb97cc52c2643d9e6b8030cec5
ealush <code@ealush.com>
2019-12-23 23:01:59 +0200

    Try unpublish

--- unpublish.js
+++ unpublish.js
@@ -7,12 +7,19 @@ const unpublish = async () => {
     const nextOnly = versions.filter((v) => v.includes('next'));
 
     const one = async (i = 0) => {
-        if (!nextOnly[i]) {
+        const current = nextOnly[i];
+
+        if (!current) {
             return;
         }
 
-        console.log(`Unpublishing: ${PACKAGE_NAME}@${nextOnly[i]}`);
-        await jsnpm.unpublish(PACKAGE_NAME, nextOnly[i]);
+        console.log(`Unpublishing: ${PACKAGE_NAME}@${current}`);
+
+        try {
+            await jsnpm.unpublish(PACKAGE_NAME, current);
+        } catch(e) {
+            console.log(`Unable to unpublish: ${current}`, e);
+        }
 
         one(++i);
     }
