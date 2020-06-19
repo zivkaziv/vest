70795947de17b6e5495d588f4be3a19db4c7198f
ealush <code@ealush.com>
2020-06-19 13:03:39 +0300

    Support dev tags

--- unpublish.js
+++ unpublish.js
@@ -1,34 +1,47 @@
-const jsnpm = require('jsnpm');
+const jsnpm = require("jsnpm");
+
+const TAG_DEV = "dev";
+const TAG_NEXT = "next";
+const VERSION_REGEXP = new RegExp([TAG_NEXT, TAG_DEV].join("|"));
 
 const unpublish = async (packageName) => {
-    const versions = await jsnpm.versions(packageName);
-    const nextOnly = versions.filter((v) => v.includes('next'));
+  const versions = await jsnpm.versions(packageName);
+  const taggedNext = await jsnpm.getVersion(packageName, TAG_NEXT);
+  const taggedDev = await jsnpm.getVersion(packageName, TAG_DEV);
 
-    const one = async (i = 0) => {
-        const current = nextOnly[i];
-
-        if (!current) {
-            return;
-        }
-
-        console.log(`Unpublishing: ${packageName}@${current}`);
-
-        try {
-            await jsnpm.unpublish(packageName, current);
-        } catch (e) {
-            console.log(`Unable to unpublish: ${current}`, e);
-        }
-
-        one(++i);
+  const nextOnly = versions.filter((v) => {
+    if ([taggedNext, taggedDev].includes(v)) {
+      return false;
     }
 
-    one();
-}
+    return !!v.match(VERSION_REGEXP);
+  });
+
+  const one = async (i = 0) => {
+    const current = nextOnly[i];
+
+    if (!current) {
+      return;
+    }
+
+    console.log(`Unpublishing: ${packageName}@${current}`);
+
+    try {
+      await jsnpm.unpublish(packageName, current);
+    } catch (e) {
+      console.log(`Unable to unpublish: ${current}`, e);
+    }
+
+    one(++i);
+  };
+
+  one();
+};
 
 const runUnpublish = async () => {
-    await unpublish('vest');
-    await unpublish('eslint-plugin-vest');
-}
+  await unpublish("vest");
+  await unpublish("eslint-plugin-vest");
+  await unpublish("n4s");
+};
 
 runUnpublish();
-
